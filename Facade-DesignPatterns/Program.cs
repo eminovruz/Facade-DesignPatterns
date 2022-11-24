682ec4551dc9583caf88e24095000d1b626d75e9
eennk <eennk@DESKTOP-8V8B7U4>
2022-11-25 00:09:54 +0400

    Add project files.

---
+++ Facade-DesignPatterns/Program.cs
@@ -0,0 +1,64 @@
+﻿#nullable disable
+
+static public class VideoDownloader
+{
+    static private VideoFormatFinder _format = new();
+    static private QualityFinder _qualify = new();
+    static private LinkFinder _link = new();
+
+    static public void Start()
+    {
+        _link.FindLink();
+        _qualify.FindIdealQuality();
+        _format.FindIdealFormat();
+        Console.WriteLine("Final Video: ");
+        Console.WriteLine(_link.Link);
+        Console.WriteLine(_qualify.Quality);
+        Console.WriteLine(_format.VideoFormat);
+    }
+}
+
+class VideoFormatFinder
+{
+    public string VideoFormat { get; set; }
+
+    public void FindIdealFormat()
+    {
+        
+        Console.WriteLine("Select Format: ");
+        Console.WriteLine(" -- mp4 ");
+        Console.WriteLine(" -- mp3 ");
+        Console.WriteLine(" -- video ");
+        VideoFormat = Console.ReadLine();
+    }
+}
+
+class QualityFinder
+{
+    public int Quality { get; set; }
+
+    public void FindIdealQuality()
+    {
+        Console.WriteLine("Select Quality: ");
+        Quality = Int32.Parse(Console.ReadLine());
+    }
+}
+
+class LinkFinder
+{
+    public string Link { get; set; }
+
+    public void FindLink()
+    {
+        Console.WriteLine("Paste link: ");
+        Link = Console.ReadLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        VideoDownloader.Start();
+    }
+}
